4d39b2f5deb1400ab2fa7fdc1011c82a749e17eb
teff-tattoos-666 <estefynovo@gmail.com>
2025-10-05 20:13:59 -0300

    Add files via upload

---
+++ Entrega2_insert_data (1).sql
@@ -0,0 +1,11 @@
+-- Inserción de datos de prueba
+INSERT INTO audiencia_diario (fecha, programa, cantidad_oyentes) VALUES
+('2025-09-01', 'Noticiero AM', 1200),
+('2025-09-01', 'Deportes Hoy', 900),
+('2025-09-01', 'Magazine Tarde', 750),
+('2025-09-02', 'Noticiero AM', 1500),
+('2025-09-02', 'Deportes Hoy', 1100),
+('2025-09-02', 'Magazine Tarde', 800),
+('2025-09-03', 'Noticiero AM', 1600),
+('2025-09-03', 'Deportes Hoy', 950),
+('2025-09-03', 'Magazine Tarde', 1000);
